bbbba796f2ce8d0cb3b53e5dcf17dc7a70b93cf2
Davidezangrando <75436804+Davidezangrando@users.noreply.github.com>
2024-12-23 19:07:50 +0100

    add security rules

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,12 +1,12 @@
-// lib/firebase.ts
+// src/lib/firebase.ts
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-    databaseURL: "https://ico-kyc-default-rtdb.firebaseio.com/",
-    // Le altre configurazioni verranno caricate dal file di credenziali
-    // Non è necessario includerle qui poiché stai usando un file di service account
-  };
-  
-  const app = initializeApp(firebaseConfig);
-  export const database = getDatabase(app);
+  databaseURL: "https://ico-kyc-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db as database };
